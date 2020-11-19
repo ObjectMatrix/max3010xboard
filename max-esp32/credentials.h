ae8e8666af6c07cdd5cdcbb8453dfbdfa42525ad
Ahm A <asad.gooogle@gmail.com>
2020-11-18 19:18:42 -0500

    Update credentials.h

--- max-esp32/credentials.h
+++ max-esp32/credentials.h
@@ -1,3 +1,7 @@
 const char* ssid = "WiFiSSID";
 const char* password = "WiFiPASSWORD";
 const char* thingSpeakAPIKey = "API-KEY";
+// configuration
+const char* mqttServer = "10.0....";
+uint16_t mqttPort = 1883;
+const char* publishedTopic = "/esp32/spO2";
